b85cb2aa38135df101163af5a423f8be10db0759
R8Zorg <hakie.notiko@gmail.com>
2025-10-20 15:24:59 +0400

    Rewrote method for moved resources

--- utils.py
+++ utils.py
@@ -5,5 +5,10 @@ def load_scaled_image(path: str, size: tuple[int, int]) -> pygame.Surface:
     return pygame.transform.scale(pygame.image.load(path).convert_alpha(), size)
 
 
-def fetch_resource(resource_name: str) -> str:
-    return "resources/" + resource_name
+def fetch_resource(resource_name: str = "", food: str = "", microwave: str = "") -> str:
+    if food:
+        return "resources/food/" + food
+    elif microwave:
+        return "resources/microwave/" + microwave
+    else:
+        return "resources/" + resource_name
